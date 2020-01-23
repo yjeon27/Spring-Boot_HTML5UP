1d5b4be25f145531d21add0c6b58729740e0c9bf
Young Jeon <yjeon27@uwo.ca>
2020-01-23 15:51:21 -0500

    template working with springboot/without spring_dispatcher_servlet/web.xml

---
+++ bootstrap-demo/src/main/java/com/example/bootstrapdemo/BootstrapDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bootstrapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapDemoApplication.class, args);
+	}
+
+}
