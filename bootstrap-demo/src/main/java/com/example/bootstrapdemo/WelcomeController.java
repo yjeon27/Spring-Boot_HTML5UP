f28241bfc6bc1437af7574a4835f58e75f60eec9
Young Jeon <yjeon27@uwo.ca>
2020-01-28 21:33:27 -0500

    pre-svg file change

--- bootstrap-demo/src/main/java/com/example/bootstrapdemo/WelcomeController.java
+++ bootstrap-demo/src/main/java/com/example/bootstrapdemo/WelcomeController.java
@@ -2,11 +2,18 @@ package com.example.bootstrapdemo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+//@RestController
 public class WelcomeController {
-    @RequestMapping("/welcome")
+    @RequestMapping("/")
     public String loginMessage(){
+        System.out.println("returning index");
         return "index";
+    }
+    @RequestMapping("/01")
+    public String pageOne(){
+        return "01";
     }
 }
